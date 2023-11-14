7e498fa8579ceb9bc33276ec895aeb6e633a7244
srca3 <santiago.cruz@alum.udep.edu.pe>
2023-11-14 08:18:07 -0500

    Problema de hilos

--- app.py
+++ app.py
@@ -3,30 +3,31 @@ import serial
 import json
 import sqlite3
 from datetime import datetime
-import random  # Importa la biblioteca para generar números aleatorios
+import random
 
 app = Flask(__name__)
 
-# Intenta configurar el puerto serial, pero si no es posible, utiliza datos aleatorios
 try:
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 except serial.serialutil.SerialException:
     ser = None
 
-# Conexión a la base de datos SQLite (crea una base de datos si no existe)
-conn = sqlite3.connect('data.db')
-cursor = conn.cursor()
+# Elimina la conexión global a la base de datos y el cursor
 
-# Crea la tabla si no existe
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS weather_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        lluvia REAL,
-        radiacion_uv REAL
-    )
-''')
-conn.commit()
+# Crea la tabla si no existe en cada solicitud
+def create_table():
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS weather_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            lluvia REAL,
+            radiacion_uv REAL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
 @app.route('/')
 def index():
@@ -34,23 +35,26 @@ def index():
 
 @app.route('/data')
 def data():
+    create_table()  # Crea la tabla antes de cada solicitud
     if ser:
-        # Leer datos del puerto serial
         data = ser.readline().decode('utf-8').strip()
         data_dict = json.loads(data)
     else:
-        # Generar datos aleatorios si no hay conexión al puerto serial
         data_dict = {
             'lluvia': random.uniform(0, 10),
             'radiacion_uv': random.uniform(0, 10)
         }
 
-    # Almacena los datos en la base de datos SQLite
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+
     cursor.execute('''
         INSERT INTO weather_data (lluvia, radiacion_uv)
         VALUES (?, ?)
     ''', (data_dict['lluvia'], data_dict['radiacion_uv']))
     conn.commit()
+
+    conn.close()
 
     return jsonify(data_dict)
 
